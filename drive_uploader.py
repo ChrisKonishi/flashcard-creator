bd47d136f978aa4790dc363deaa2564fb708ea8c
Chris Konishi <christian.konishi@gmail.com>
2024-09-04 10:40:40 -0300

    fix: Delete old file before uploading new file to Google Drive

--- drive_uploader.py
+++ drive_uploader.py
@@ -1,5 +1,6 @@
 from pydrive2.auth import GoogleAuth
 from pydrive2.drive import GoogleDrive
+import os
 
 class DriveUploader:
     def __init__(self, credential_file: str, directory_id: str) -> None:
@@ -17,9 +18,19 @@ class DriveUploader:
         gauth = GoogleAuth(settings=settings)
         gauth.ServiceAuth()
         return gauth
+    
+    def _delete_file(self, file, drive):
+        file = drive.CreateFile({'id': file['id']})
+        file.Delete()
 
     def __call__(self, filepath):
         drive = GoogleDrive(self.gauth)
+        file_list = drive.ListFile({'q': f"'{self.directory_id}' in parents and trashed=false"}).GetList()
+        for file in file_list:
+            if file['title'] == os.path.basename(filepath):
+                print(f'Deleting old file {file["title"]}')
+                self._delete_file(file, drive)
+
         file = drive.CreateFile({'parents': [{'id': self.directory_id}]})
         file.SetContentFile(filepath)
         file.Upload()
